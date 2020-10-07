de9ae480f0685a497c4b2026aac5face21e72a66
BraenaanV <braenaan.vaughan@gmail.com>
2020-10-07 18:34:24 -0400

    push draft 1 of app

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# README-Generator
+# README Generator
+  ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/undefined/undefined?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/undefined/undefined?style=flat&logo=appveyor)
+  
+  Check out the badges hosted by [shields.io](https://shields.io/).
+  
+  
+  ## Description 
+  
+  A command line interface that generates a README against a set of user inputs
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Contributing](#contributing)
+  * [License](#license)
+  
+  ## Installation
+  
+  *Steps required to install project:*
+  
+  undefined
+  
+  ## License
+  
+  MIT License
+  
--- index.js
+++ index.js
@@ -1,16 +1,102 @@
-// array of questions for user
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const api = require("./utils/api");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 const questions = [
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title",
+        default: "My Title",
+    },
+
+    {
+        type: "input",
+        message: "Enter project description",
+        name: "description",
+        default: "My Description"
+    },
+
+    {
+        type: "input",
+        message : "Enter installation instructions",
+        name: "install",
+        default: "Installation Instructions",
+    },
+
+    {
+        type: "input",
+        message: "What is your GitHub username (exclude the @)",
+        name: "GitHub-Username",
+        default: "My Username",
+    },
+
+    {
+        type: "input",
+        message: "What is the name of your GitHub repo?",
+        name: "GitHubRepo",
+        default: "New-Repo",
+    },
+
+    {
+        type: "input",
+        message: "Enter some examples of the project for the usage section!",
+        name: "usage"
+    },
+
+    {
+        type: "list",
+        message: "Choose a license for your project!",
+        choices: ["MIT License","GNU Lesser General Public License v3.0","Mozilla Public License 2.0","GNU Affero General Public License v3.0","The unlicense","Apache License 2.0","GNU General Public License v3.0"],
+        name: "license"
+    },
+
+    {
+        type: "input",
+        message: "Enter instructions for others to contribute!",
+        name: "contributors"
+    },
+
+    {
+        type: "input",
+        message: "Enter any tests you wrote for your application & how to run them here!",
+        name: "tests"
+    }
+
 
 ];
 
-// function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Congratulations!! Your README file has been generated!!")
+    });
 }
 
-// function to initialize program
-function init() {
+const writeFileAsync = util.promisify(writeToFile);
+
+async function init() {
+    try {
+        const userResponses = await inquirer.prompt(questions);
+        console.log("You Entered: ", userResponses);
+    
+        const userInfo = await api.getUser(userResponses);
+        console.log("Your GitHub user info: ", userInfo);
+    
+        console.log("Generating README")
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+    
+        await writeFileAsync('README.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
 
 }
-
-// function call to initialize program
 init();
